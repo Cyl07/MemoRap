e522e4e7a5588467822e99e54b8e2cda500bba84
Cyl <labetowiez@aol.fr>
2021-11-26 00:42:07 +0100

    begin lobby

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,10 +2,10 @@ var conn = new WebSocket('ws://localhost:8080');
 conn.onopen = function(e) {
     console.log("Connection established!");
 };
-/*
+
 conn.onmessage = function(e) {
     console.log(e.data);
-};*/
+};
 
 let button = document.getElementById("submit");
 
